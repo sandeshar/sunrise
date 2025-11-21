1c3854d70e5d32d3398c5d9a69e33a3ce9e7ee06
aryal-sandesh <sandesharyal.135@gmail.com>
2025-11-21 17:56:18 +0545

    Add creative universe title to gallery component

--- app/components/Gallery.tsx
+++ app/components/Gallery.tsx
@@ -4,6 +4,8 @@ const Gallery = () => {
     return (
         <div className="pt-20">
             <SectionTitle title="Gallery" />
+            <p className="text-center text-6xl font-bold uppercase">Creative Universe</p>
+
         </div>
     );
 };
