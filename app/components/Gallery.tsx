9eb4fa2aff60f6c9c3f6f33040c6f4a6e0671474
aryal-sandesh <sandesharyal.135@gmail.com>
2025-11-23 11:16:41 +0545

    Add marquee animation and enhance Gallery component layout with project details

--- app/components/Gallery.tsx
+++ app/components/Gallery.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import SectionTitle from "./helper/SectionTitle";
+import { FaCircle } from "react-icons/fa";
 
 const Gallery = () => {
     const images = [
@@ -13,9 +14,9 @@ const Gallery = () => {
             <SectionTitle title="Gallery" />
             <p className="text-center text-6xl font-bold uppercase">Creative Universe</p>
 
-            <div className="px-10 py-10 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-3 justify-items-center">
+            <div className="px-5 pt-10 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-3 justify-items-center">
                 {images.map((src, index) => (
-                    <div key={index} className="relative group overflow-hidden rounded-lg shadow-lg">
+                    <div key={index} className="relative group overflow-hidden">
                         <Image
                             src={src}
                             alt={`Gallery Image ${index}`}
@@ -30,6 +31,34 @@ const Gallery = () => {
                         </div>
                     </div>
                 ))}
+            </div>
+            <div className="mt-5 grid grid-cols-2 px-5 gap-3">
+                <div className="space-y-4">
+                    <p className="text-6xl font-bold uppercase"><span className="bg-yellow-400 px-2">All</span> Projects</p>
+                    <p className="text-gray-400 text-lg font-medium">Explore our diverse collection of creative works. From web design to digital art, our portfolio showcases a wide range of styles and techniques. Training and innovation are at the heart of what we do. We provide all kinds of digital and printing services that your business needs.</p>
+                    <a className="border-2 border-red-600 px-4 py-2 font-bold hover:cursor-pointer flex justify-center items-center space-x-2 w-max"><FaCircle size={15} className="fill-red-600" /><p>View All</p></a>
+
+                </div>
+                <div className="flex space-x-2">
+                    <Image
+                        src={'/art1.jpg'}
+                        alt={`Gallery Image`}
+                        width={200}
+                        height={200}
+                        className="object-cover w-full h-full transition-transform duration-500 group-hover:scale-105"
+                    /><Image
+                        src={'/art1.jpg'}
+                        alt={`Gallery Image`}
+                        width={200}
+                        height={200}
+                        className="object-cover w-full h-full transition-transform duration-500 group-hover:scale-105"
+                    />
+                </div>
+            </div>
+            <div className="bg-red-600 text-white font-bold py-1 uppercase mt-10 -rotate-2 overflow-hidden whitespace-nowrap">
+                <div className="animate-marquee">
+                    <span className="pr-20">In every pixel, there is a story waiting to be told. So let us help you tell yours.</span>
+                </div>
             </div>
         </div>
     );
