dc4740dfa15ee90709926d2b903b141a088d9d94
aryal-sandesh <sandesharyal.135@gmail.com>
2025-11-26 07:32:33 +0545

    Refactor Footer component layout for improved structure and styling

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -2,10 +2,30 @@ import Image from "next/image";
 
 const Footer = () => {
     return (
-        <div className="grid grid-cols-3 ">
-            <div className="flex flex-col justify-center items-center"><Image src={'/sunriselogo.png'} alt="Sunrise Logo" width={100} height={100} /></div>
-            <div></div>
-            <div></div>
+        <div className="grid grid-cols-4 mt-20 py-10 px-20 bg-gray-100 gap-10">
+            <div className="flex flex-col justify-center items-center col-span-2">
+                <Image src={'/sunriselogo.png'} alt="Sunrise Logo" width={200} height={200} />
+                <p className="mt-4 text-gray-600">© 2024 Sunrise Designs. All rights reserved.</p>
+                <p>Designed by <a href="https://aryalsandesh.com.np" target="_blank" rel="noopener noreferrer" className="text-blue-500 hover:underline font-bold">Sandesh Aryal</a></p>
+            </div>
+            <div>
+                <p className="font-bold">Services</p>
+                <ul className="list-none space-y-2 mt-2">
+                    <li>Logo Design</li>
+                    <li>Brand Identity</li>
+                    <li>Printing Designs</li>
+                    <li>Digital Marketing</li>
+                    <li>Training Classes</li>
+                </ul>
+            </div>
+            <div>
+                <p className="font-bold">Contact Us</p>
+                <ul className="list-none space-y-2 mt-2">
+                    <li>Email: sunriseprint07@gmail.com</li>
+                    <li>Phone: +014240119</li>
+                    <li>Address: Aadwait Marg, Bagbazar, Kathmandu</li>
+                </ul>
+            </div>
         </div>
     );
 };
