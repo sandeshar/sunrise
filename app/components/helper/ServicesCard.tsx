0fcf84b1889b051923b34740c3c1d9fe334269a8
aryal-sandesh <sandesharyal.135@gmail.com>
2026-02-06 08:58:53 +0545

    Refactor ServicesCard component for improved layout and styling; enhance hover effect and add title and description display

--- app/components/helper/ServicesCard.tsx
+++ app/components/helper/ServicesCard.tsx
@@ -10,22 +10,28 @@ interface ServicesCardProps {
 
 const ServicesCard = ({ Icon, title, description, points }: ServicesCardProps) => {
     return (
-        <div className="p-5 shadow-[0_4px_15px_rgba(0,0,0,0.2)] flex flex-col items-center text-center h-full w-full">
-            <div className="flex flex-col items-center text-center space-y-2">
-                <Icon size={40} className="fill-red-600" />
-                <p className="font-bold text-2xl">{title}</p>
-                <p className="text-gray-600">{description}</p>
+        <div className="group relative bg-white border border-slate-200 p-10 rounded-2xl hover:border-primary/40 transition-all duration-300 card-hover-gradient">
+            <div className="mb-8 flex justify-center">
+                <div className="relative">
+                    <Icon size={48} className=" text-primary transition-colors duration-300" />
+                    <div className="absolute -bottom-1 left-1/2 -translate-x-1/2 w-8 h-1 bg-primary/10 group-hover:bg-primary/20 rounded-full transition-all duration-300"></div>
+                </div>
             </div>
-            <ul className="list-none space-y-3 mt-4 w-full">
+            <div className="text-center mb-10">
+                <h2 className="text-2xl font-bold mb-3 text-slate-800">{title}</h2>
+                <p className="text-slate-500 text-sm leading-relaxed max-w-[300px] mx-auto">
+                    {description}
+                </p>
+            </div>
+            <ul className="space-y-5">
                 {points.map((point, index) => (
-                    <li key={index} className="flex items-center">
-                        <FaCheck className="text-red-600 mr-2" />
-                        <span>{point}</span>
+                    <li key={index} className="flex items-center gap-3 text-sm font-medium text-slate-600">
+                        <FaCheck className="text-primary text-xl font-bold" />
+                        {point}
                     </li>
                 ))}
             </ul>
         </div>
-
     );
 };
 
